d7d6ec17379056909ace8a350f8b16f88ace7948
Michelle Rodríguez <promichfsd@gmail.com>
2024-12-20 01:19:53 -0300

    testimonio de santi y ajustes de estilo en benefits service button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
         <Testimonial 
          imgSrc='./images/santiago-dominguez.png' 
          altText="Santiago Dominguez" 
-         testimonial="Estamos muy satisfechos con los resultados de mishi systems y su impacto en nuestra empresa fue puramente positivo" 
+         testimonial="Tuve el placer de trabajar con Mishi Systems quienes demostraron ser profesionales totalmente organizados y comprometidos. Su dedicación al proyecto fue total, cumpliendo con todas las entregas y solicitudes en cada etapa. Siempre mostraron una actitud proactiva, dispuesta a colaborar y a encontrar formas de mejorar los resultados. Sin dudas recomiendo sus servicios." 
          name="Santiago Dominguez" 
          company="T2Company"
          rol="Gerente de Marketing" />
